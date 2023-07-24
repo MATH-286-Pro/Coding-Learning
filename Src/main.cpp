573de15e97cb7c8bef7195bf5021806a73512cd6
Daud-117 <1299065753@qq.com>
2023-07-24 22:48:39 +0800

    2023.7.24 First Test of git

---
+++ Src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    std::cout <<"Hello World"<< std::endl;
+
+    cout <<"a="<< a << endl;
+
+    system("pause");
+
+    return 0;
+}
